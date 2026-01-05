1b1e251a09d4d1097b4d4defa56babfd870ef152
AU-TOW <gavin@autow-services.co.uk>
2026-01-05 18:39:24 +0000

    [MK3] Implementation: Preview Component: components/smart-jotter/types.ts

---
+++ components/smart-jotter/types.ts
@@ -0,0 +1,9 @@
+// Re-export types for easier imports within smart-jotter components
+export type {
+  ParsedBookingData,
+  FieldConfidence,
+  OCRResponse,
+  ParseResponse,
+  SmartJotterState,
+  CanvasData
+} from '../../types/smart-jotter';
